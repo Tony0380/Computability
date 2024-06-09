8b8bbbe2c9017125f86e3fdd34066da86a5cd9e2
Antonio Colamartino <32678450+Tony0380@users.noreply.github.com>
2024-06-09 13:55:08 +0200

    Added DFA class

---
+++ app/src/main/java/computability/CalculationModels/DFA/State.java
@@ -0,0 +1,39 @@
+package computability.CalculationModels.DFA;
+import computability.DataStructures.Graph.Node;
+public class State extends Node{
+
+    private boolean accepting;
+
+    /**
+     * Create a new state with the given name.
+     * @param name The name of the state.
+     */
+    public State(String name) {
+        super(name);
+        this.accepting = false;
+    }
+
+    /**
+     * Check if the state is accepting.
+     * @return True if the state is accepting, false otherwise.
+     */
+    public boolean isAccepting() {
+        return accepting;
+    }
+
+    /**
+     * Set whether the state is accepting.
+     * @param accepting True if the state is accepting, false otherwise.
+     */
+    public void setAccepting(boolean accepting) {
+        this.accepting = accepting;
+    }
+
+    /**
+     * Get the name of the state.
+     * @return The name of the state.
+     */
+    public String getName() {
+        return super.getName();
+    }
+}
---
+++ app/src/main/java/computability/CalculationModels/DFA/Transiction.java
@@ -0,0 +1,94 @@
+package computability.CalculationModels.DFA;
+
+public class Transiction{
+
+    private State start;
+    private State end;
+    private char symbol;
+
+    /**
+     * Create a new transiction between the given states with the given symbol.
+     * @param start The start state of the transiction.
+     * @param end
+     * @param symbol
+     * @return The transiction that was added.
+     */
+    public Transiction(State start, State end, char symbol) {
+        this.start = start;
+        this.end = end;
+        this.symbol = symbol;
+    }
+
+    /**
+     * Get the start state of the transiction.
+     * @return The start state of the transiction.
+     */
+    public State getStart() {
+        return start;
+    }
+
+    /**
+     * Get the end state of the transiction.
+     * @return The end state of the transiction.
+     */
+    public State getEnd() {
+        return end;
+    }
+
+    /**
+     * Get the symbol of the transiction.
+     * @return The symbol of the transiction.
+     */
+    public char getSymbol() {
+        return symbol;
+    }
+
+    /**
+     * Set the symbol of the transiction.
+     * @param symbol The symbol of the transiction.
+     */
+    public void setSymbol(char symbol) {
+        this.symbol = symbol;
+    }
+
+    /**
+     * Set the start state of the transiction.
+     * @param start The start state of the transiction.
+     */
+    public void setStart(State start) {
+        this.start = start;
+    }
+
+    /**
+     * Set the end state of the transiction.
+     * @param end The end state of the transiction.
+     */
+    public void setEnd(State end) {
+        this.end = end;
+    }
+
+    /**
+     * Get the name of the transiction.
+     * @return The name of the transiction.
+     */
+    public String getName() {
+        return start.getName() + " -> " + end.getName() + " on " + symbol;
+    }
+
+    /**
+     * Get the string representation of the transiction.
+     * @return The string representation of the transiction.
+     */
+    @Override
+    public String toString() {
+        return "δ : " + start.getName() + " x " + symbol + " -> " + end.getName();
+    }
+
+    /**
+     * Check if the transiction is valid.
+     * @return True if the transiction is valid, false otherwise.
+     */
+    public boolean isValid() {
+        return start != null && end != null && symbol != '\0';
+    }
+}
