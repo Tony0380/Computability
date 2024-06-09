0a6b8f55e3baee11aceb559c1485d6879793ebb2
Antonio Colamartino <32678450+Tony0380@users.noreply.github.com>
2024-06-09 15:27:01 +0200

    Added NON-determinstic check on new transictions

--- app/src/main/java/computability/CalculationModels/DFA/State.java
+++ app/src/main/java/computability/CalculationModels/DFA/State.java
@@ -36,4 +36,18 @@ public class State extends Node{
     public String getName() {
         return super.getName();
     }
+
+    /**
+     * Check if the state is equal to another state.
+     * @param o The state to compare to.
+     * @return True if the states are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof State) {
+            State s = (State) o;
+            return this.getName().equals(s.getName());
+        }
+        return false;
+    }
 }
