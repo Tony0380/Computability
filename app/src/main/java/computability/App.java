5118bd1d9a11192315a18901aca975293219e35b
Antonio Colamartino <colamar95@outlook.it>
2024-11-02 11:04:23 +0100

    refactor: remove getGreeting method and update tests for main method

--- app/src/main/java/computability/App.java
+++ app/src/main/java/computability/App.java
@@ -5,9 +5,6 @@ package computability;
 
 public class App {
 
-    public String getGreeting() {
-        return "Hello world.";
-    }
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.run();
